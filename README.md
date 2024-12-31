390f8aab19543b8f56efafb1fb35c7cf9e88b613
samenhaus <admin@samenhaus.de>
2024-12-31 02:56:13 -0300

    RC 1

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # deluge-song-midi-mappings
 get midi CC mappings from a deluge song XML
+
+try here: https://dev.jackel.xyz/deluge/song-midi-mappings/
--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function handleFiles() {
     this.value = "";
     return;
   }
- 
+
   const reader = new FileReader();
   reader.addEventListener(
     "load",
@@ -27,18 +27,15 @@ function handleFiles() {
       var doc = new DOMParser();
       var xmlstring = doc.parseFromString(contents, "text/xml");
       const result = xmlstring.evaluate('//midiKnob', xmlstring, null, XPathResult.ORDERED_NODE_ITERATOR_TYPE, null);
-    
+      
       try {
         let node = result.iterateNext();
         output.innerText = "";
         while (node) {
-          //console.log("node", node);
-
           myObjArr = Array.prototype.slice.call(node.attributes);
           myStrArr = myObjArr.map(function(item){return item.name+'='+item.value})
           nodeText = JSON.stringify(myStrArr)
-
-
+          
           if(node.parentNode.parentElement.getAttribute("name") != null) {
             nodeParent = "[KIT] " + node.parentNode.parentElement.getAttribute("name");
           }
@@ -47,8 +44,8 @@ function handleFiles() {
             nodeParent = "[SYNTH] " + node.parentNode.parentElement.getAttribute("presetName");
           }
 
-          //console.log(node.parentNode.parentElement);
           output.insertAdjacentHTML("afterend", "<strong>" + nodeParent + ":</strong> " + nodeText + "<br><br>"); 
+          //output.append("<strong>" + nodeParent + ":</strong> " + nodeText + "<br><br>"); 
           node = result.iterateNext();
         }
       } catch (e) {
@@ -60,5 +57,4 @@ function handleFiles() {
 
   console.log("loading file: " + file.name + "\ntype: " + file.type);
   reader.readAsText(file);
-
 }
